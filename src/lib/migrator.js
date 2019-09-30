a959880d160f560850dd33a81f7307a103e088fa
Lewis Daly <lewisdaly@me.com>
2019-09-30 16:25:18 +0800

    Adding migrator tests

--- src/lib/migrator.js
+++ src/lib/migrator.js
@@ -9,6 +9,7 @@ const updateMigrationsLocation = (kf) => {
   kf.migrations.directory = Path.join(process.cwd(), parsedMigrationDir.base)
   const parsedSeedsDir = Path.parse(kf.seeds.directory)
   kf.seeds.directory = Path.join(process.cwd(), parsedSeedsDir.base)
+  console.log("kf is", kf)
   return kf
 }
 
